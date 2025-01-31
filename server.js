f4627fde59999d512bdc5106933af35bb74e1089
Angelo Lustosa de Figueiredo Filho <Angelo.Filho@digitalcollege.internal>
2025-01-30 21:01:35 -0300

    Configurando o nodemon express dotenv

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+const HOST = '127.0.0.1';
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Olá Mundo!')
+})
+
+app.listen(PORT, HOST, () => {
+    console.log(`App de exemplo está rodando em http://${HOST}:${PORT}`);
+  });
